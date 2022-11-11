bca6a9f5b3aef513bc694ddf4d31ad5f745867f9
A24-N <117143210+A24-N@users.noreply.github.com>
2022-11-11 05:10:28 +0000

    [add]users_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   root to: "homes#top"
   devise_for :users
+
+  resources :books, only: [:new, :show, :index]
+  resources :users, only: [:show, :edit, :index]
 
 end
